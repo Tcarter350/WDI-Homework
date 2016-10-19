e2960875fd682b3f98852984d5670a5e8485eac3
Thea Carter <theacarter@ymail.com>
2016-10-19 19:00:35 +0100

    new profiles

--- Tcarter350/W04D03/tindersaurus/config/routes.js
+++ Tcarter350/W04D03/tindersaurus/config/routes.js
@@ -1,6 +1,14 @@
+const Profiles = require('../models/profiles');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => res.render('index'));
 
 module.exports = router;
+
+router.get('/profiles', (req, res) => {
+  Profiles.find({}, (err, quotes) => {
+    if(err) return res.status(500).send("500: Server Error");
+    res.render('profiles/index', { quotes });
+  });
+});
