20b3e2a7b80d597faa38906b405e828597ce231a
Thea Carter <theacarter@ymail.com>
2016-11-10 10:59:32 +0000

    criminal app hw

---
+++ Tcarter350/W07D03/criminals-app/public/js/criminalsController.js
@@ -0,0 +1,45 @@
+angular.module('criminalsApp')
+  .controller('CriminalsController', CriminalsController);
+
+CriminalsController.$inject = ['$http'];
+function CriminalsController($http) {
+  const criminals = this;
+  criminals.all = [];
+  criminals.addCriminal = addCriminal;
+  criminals.removeCriminal = removeCriminal;
+
+  criminals.newCriminal = {};
+
+  function addCriminal() {
+
+    $http.post('/criminals', criminals.newCriminal)
+      .then((res) => {
+        criminals.all.push(res.data);
+        criminals.newCriminal = {};
+
+        criminals.form.$setPristine();
+        criminals.form.$setUntouched();
+      })
+      .catch(() => {
+        console.log('Something went wrong!');
+      });
+
+  }
+
+  function getCriminals() {
+    $http.get('/criminals')
+      .then((res) => {
+        criminals.all = res.data;
+      });
+  }
+
+  function removeCriminal(id, $index) {
+    $http.delete(`/criminals/${id}`)
+      .then(() => {
+        criminals.all.splice($index, 1);
+      });
+  }
+
+  getCriminals();
+
+}
